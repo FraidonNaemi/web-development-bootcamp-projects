68dd267d92c89933e703534e7b339b495af88eb8
fraidon <fraidonkhan1906@gmail.com>
2023-08-03 15:01:45 +1000

    Complete section 23 - Node.js

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,12 @@
+/*var generateName = require("sillyname");
+
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log(`The name is ${sillyName}`);*/
+
+import superheroes from "superheroes";
+
+const name = superheroes.random();
+
+console.log(`Superhero ${name}!`);
